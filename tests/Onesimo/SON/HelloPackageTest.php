4dcdf11b59e08bef279a3fe31768a1494b22dcfb
Onesimo Batista <onesimobatista@gmail.com>
2016-09-16 00:27:23 -0300

    last commit course

---
+++ tests/Onesimo/SON/HelloPackageTest.php
@@ -0,0 +1,14 @@
+<?php 
+namespace Onesimo\SON;
+
+class HelloPackageTest extends \PHPUnit_Framework_TestCase
+{
+	public function testGetHello()
+	{
+		$hello = new HelloPackage();
+		$string = $hello->getHello();
+		$this->assertNotNull($string);
+		$this->assertInternalType('string',$string);
+		$this->assertNotEquals(0, strlen($string));
+	}
+}
